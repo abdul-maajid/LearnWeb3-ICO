911cbc197ebcc650221f9808a6a00fbdaa13d9aa
abdul-maajid <amaajid0@gmail.com>
2022-08-29 14:44:34 +0500

    Done changes

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the NFT Contract that you deployed
 const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
-  "0xf63907113EF4eb6A2086fCB5ac2A5Dcabe782f20";
+  "0xC8349C66738f97Fa68410403eee0eB0C47F0134D";
 
 module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
--- hardhat/contracts/CryptoDevToken.sol
+++ hardhat/contracts/CryptoDevToken.sol
@@ -7,7 +7,7 @@ import "./ICryptoDevs.sol";
 
 contract CryptoDevToken is ERC20, Ownable {
     // Price of one Crypto Dev token
-    uint256 public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokenPrice = 0.00001 ether;
     // Each NFT would give the user 10 tokens
     // It needs to be represented as 10 * (10 ** 18) as ERC20 tokens are represented by the smallest denomination possible for the token
     // By default, ERC20 tokens have the smallest denomination of 10^(-18). This means, having a balance of (1)
